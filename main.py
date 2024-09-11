022e317287bfca6a091f7d7ab866d0e42739039f
olovff <121008869+olovff@users.noreply.github.com>
2024-09-11 22:14:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# "1st program"
+print((9 ** 0.5) * 5)
+
+# "2st program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3st program"
+int1 = 2 * 2 + 2
+print(int1)
+int2 = 2 * (2 + 2)
+print(int2)
+print(int1 == int2)
+
+# "4st program"
+string = '123.456'
+string = float(string) * 10
+print(int(string % 10))
